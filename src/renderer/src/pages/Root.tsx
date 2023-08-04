13aa863a0c739ca8f550bb83eefb4b544ef44f0f
lukebrichey <lukebrichey@gmail.com>
2023-08-04 12:58:44 -0400

    style: Removed border on title div

--- src/renderer/src/pages/Root.tsx
+++ src/renderer/src/pages/Root.tsx
@@ -7,7 +7,7 @@ export default function Home(): JSX.Element {
 
   return (
     <div className="min-h-screen w-screen bg-blue-200 pt-10 flex flex-col">
-      <div className="bg-white w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-xl text-center p-6 mx-auto shadow-lg border-black border">
+      <div className="bg-white w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 rounded-xl text-center p-6 mx-auto shadow-lg">
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-semibold italic">Flow</h1>
       </div>
       <div className="bg-white w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/2 rounded-md text-center p-4 sm:p-8 md:p-12 lg:p-16 mt-20 mx-auto shadow-lg">
