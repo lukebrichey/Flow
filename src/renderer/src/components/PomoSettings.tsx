ab8664d0608a67419fff52fc265827a5592f4e3c
lukebrichey <lukebrichey@gmail.com>
2023-08-10 10:28:47 -0400

    feat: Added pomodoro count setting

--- src/renderer/src/components/PomoSettings.tsx
+++ src/renderer/src/components/PomoSettings.tsx
@@ -10,12 +10,14 @@ export function PomoSettings({ preferences, onSave }: PomoSettingsProps): JSX.El
   const [settings, setSettings] = useState<IPreference>({
     pomodoroLength: preferences.pomodoroLength,
     shortBreakLength: preferences.shortBreakLength,
-    longBreakLength: preferences.longBreakLength
+    longBreakLength: preferences.longBreakLength,
+    pomodoroCount: preferences.pomodoroCount
   });
   const [errorMessages, setErrorMessages] = useState<{ [key: string]: string | null }>({
     pomodoroLength: null,
     shortBreakLength: null,
-    longBreakLength: null
+    longBreakLength: null,
+    pomodoroCount: null
   });
 
   // Fetch preferences from the database
@@ -45,7 +47,8 @@ export function PomoSettings({ preferences, onSave }: PomoSettingsProps): JSX.El
       id: 1,
       pomodoroLength: settings.pomodoroLength,
       shortBreakLength: settings.shortBreakLength,
-      longBreakLength: settings.longBreakLength
+      longBreakLength: settings.longBreakLength,
+      pomodoroCount: settings.pomodoroCount
     };
     db.preferences.put(updatedPreferences);
     onSave(updatedPreferences);
@@ -103,6 +106,23 @@ export function PomoSettings({ preferences, onSave }: PomoSettingsProps): JSX.El
           }}
         />
         <span className="text-red-500">{errorMessages.longBreakLength}</span>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <label className="text-gray-700 text-sm font-bold mb-2" htmlFor="pomodoroCount">
+          Pomodoro Count
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="pomodoroCount"
+          type="number"
+          value={settings.pomodoroCount}
+          onChange={(e): void => {
+            const value = parseInt(e.target.value);
+            validateInput(value, 'pomodoroCount') &&
+              setSettings({ ...settings, pomodoroCount: value });
+          }}
+        />
+        <span className="text-red-500">{errorMessages.pomodoroCount}</span>
       </div>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
