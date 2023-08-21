82760eeb4f096f1dde90e7b3f0cc01eda2d6c609
lukebrichey <lukebrichey@gmail.com>
2023-08-21 13:59:13 -0400

    feat: Added logic to handle streaks along with helper functions
    - changed getFocusTime and updateFocusTime to get latest entry by date
    - Added some comments to help describe db structure

--- src/renderer/src/lib/db.ts
+++ src/renderer/src/lib/db.ts
@@ -15,14 +15,19 @@ interface IFocusStat {
   totalFocusTime: number;
   weeklyFocusTime?: number;
   monthlyFocusTime?: number;
+  focusStreak: number; // Current number of consecutive days the user has met their focus goal
 }
 
-// Interface for daily focus goals and streaks
+/* Interface for daily focus goals and streaks
+ We create a new entry for each day and update that accordingly, to allow for
+ easy stat tracking and to allow the user to see their progress over time. It 
+ also makes the streak calculation easier, as we can just check the latest entry
+ */
 interface IDailyFocusGoal {
   id?: number;
   date: Date;
   focusGoal: number; // Number of minutes the user wants to focus for each day
-  focusStreak: number; // Number of days in a row that the user has met their focus goal
+  focusTime: number; // Number of minutes the user has focused for today
 }
 
 const DEFAULT_PREFERENCES: IPreference = {
@@ -32,13 +37,23 @@ const DEFAULT_PREFERENCES: IPreference = {
   pomodoroCount: 4
 };
 
+const todayAtMidnight = new Date();
+todayAtMidnight.setHours(0, 0, 0, 0);
+
 const DEFAULT_FOCUS_GOAL: IDailyFocusGoal = {
-  date: new Date(),
+  date: todayAtMidnight,
   focusGoal: 120, // 2 hours
-  focusStreak: 0
+  focusTime: 0 // 0 minutes
 };
 
 // No need for default values for focus stats, as they are updated on the fly
+
+/*
+  Database tables are as follows:
+  preferences: Stores the user's pomodoro preferences, single entry
+  focusStats: Stores the user's focus stats, single entry
+  dailyFocusGoals: Stores the user's daily focus goals and times, one entry per day
+*/
 
 class MyDatabase extends Dexie {
   // Add definite assignment assertion
@@ -51,8 +66,8 @@ class MyDatabase extends Dexie {
 
     this.version(1).stores({
       preferences: '++id, pomodoroLength, shortBreakLength, longBreakLength, pomodoroCount',
-      focusStats: '++id, totalFocusTime, weeklyFocusTime, monthlyFocusTime',
-      dailyFocusGoals: '++id, date, focusGoal, focusStreak'
+      focusStats: '++id, totalFocusTime, weeklyFocusTime, monthlyFocusTime, focusStreak',
+      dailyFocusGoals: '++id, date, focusGoal, focusTime'
     });
 
     this.preferences = this.table('preferences');
@@ -85,45 +100,54 @@ export const getPreferences = async (): Promise<IPreference> => {
 
 // Function to get the latest focus time
 export const getFocusTime = async (): Promise<number> => {
-  const latestStat = await db.focusStats.orderBy('id').reverse().first();
-  return latestStat?.totalFocusTime || 0;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+
+  const todaysGoal = await db.dailyFocusGoals.where('date').equals(today).first();
+  return todaysGoal?.focusTime || 0;
 };
 
 // Function to update the focus time
 export const updateFocusTime = async (newFocusTime: number): Promise<void> => {
-  await db.focusStats.update(1, { totalFocusTime: newFocusTime });
-};
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
 
-// Function to get the latest focus goal
-export const getFocusGoal = async (): Promise<number> => {
-  const latestGoal = await db.dailyFocusGoals.orderBy('id').reverse().first();
-  return latestGoal?.focusGoal || DEFAULT_FOCUS_GOAL.focusGoal;
-};
-
-// Function to update the focus goal
-export const updateFocusGoal = async (newFocusGoal: number): Promise<void> => {
-  await db.dailyFocusGoals.update(1, { focusGoal: newFocusGoal });
+  const todaysGoal = await db.dailyFocusGoals.where('date').equals(today).first();
+  if (todaysGoal && todaysGoal.id) {
+    await db.dailyFocusGoals.update(todaysGoal.id, { focusTime: newFocusTime });
+  }
 };
 
 // Function to get the latest focus streak
 export const getFocusStreak = async (): Promise<number> => {
-  const latestGoal = await db.dailyFocusGoals.orderBy('id').reverse().first();
-  return latestGoal?.focusStreak || DEFAULT_FOCUS_GOAL.focusStreak;
+  const latestGoal = await db.focusStats.orderBy('id').reverse().first();
+  return latestGoal?.focusStreak || 0;
 };
 
-// Function to update the focus streak
-export const updateFocusStreak = async (newFocusStreak: number): Promise<void> => {
-  await db.dailyFocusGoals.update(1, { focusStreak: newFocusStreak });
-};
+// Fucntion that handles the focus streak logic, will be called at the beginning
+// of each new day
+export const updateFocusStreak = async (): Promise<void> => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-export const incrementFocusStreak = async (currentFocusTime: number): Promise<void> => {
-  const focusGoal = await getFocusGoal();
-  const focusStreak = await getFocusStreak();
-  if (currentFocusTime >= focusGoal) {
-    await updateFocusStreak(focusStreak + 1);
+  const latestGoal = await db.dailyFocusGoals.orderBy('date').reverse().first();
+
+  // Check if there are missing days
+  if (latestGoal && latestGoal.date) {
+    const lastDate = new Date(latestGoal.date);
+    const daysDifference = (today.getTime() - lastDate.getTime()) / (1000 * 60 * 60 * 24);
+
+    if (daysDifference > 1) {
+      // Reset streak if there are missing days
+      await db.focusStats.update(1, { focusStreak: 0 });
+    } else if (latestGoal.focusTime >= latestGoal.focusGoal) {
+      // Increment streak if goal met
+      const focusStreak = await getFocusStreak();
+      await db.focusStats.update(1, { focusStreak: focusStreak + 1 });
+    }
   }
 };
 
 // Use 'export type' for re-exporting types
-export type { IPreference, IFocusStat };
+export type { IPreference, IFocusStat, IDailyFocusGoal };
 export { db };
