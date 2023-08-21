5270f84c72c194c9eae31015b2ea5a0be7525948
lukebrichey <lukebrichey@gmail.com>
2023-08-21 09:34:22 -0400

    refactor: Moved defaults into db constructor
    - Added interfaces and new tables for focusStats and dailyFocusGoals
    - Simplified getPreferences function
    - Fixed updateFocusTime to comply with interface and table changes

--- src/renderer/src/lib/db.ts
+++ src/renderer/src/lib/db.ts
@@ -1,22 +1,5 @@
 import Dexie from 'dexie';
 
-class MyDatabase extends Dexie {
-  // Add definite assignment assertion
-  preferences!: Dexie.Table<IPreference, number>;
-  focusStats: Dexie.Table<IFocusStat, number>;
-
-  constructor() {
-    super('MyDatabase');
-
-    this.version(1).stores({
-      preferences: '++id, pomodoroLength, shortBreakLength, longBreakLength, pomodoroCount',
-      focusStats: '++id, totalFocusTime'
-    });
-
-    this.focusStats = this.table('focusStats');
-  }
-}
-
 // Interface for pomodoro preferences
 interface IPreference {
   id?: number;
@@ -30,6 +13,51 @@ interface IPreference {
 interface IFocusStat {
   id?: number;
   totalFocusTime: number;
+  weeklyFocusTime?: number;
+  monthlyFocusTime?: number;
+}
+
+// Interface for daily focus goals and streaks
+interface IDailyFocusGoal {
+  id?: number;
+  date: Date;
+  focusGoal: number;
+  focusStreak: number;
+}
+
+const DEFAULT_PREFERENCES: IPreference = {
+  pomodoroLength: 30,
+  shortBreakLength: 5,
+  longBreakLength: 15,
+  pomodoroCount: 4
+};
+
+class MyDatabase extends Dexie {
+  // Add definite assignment assertion
+  preferences: Dexie.Table<IPreference, number>;
+  focusStats: Dexie.Table<IFocusStat, number>;
+  dailyFocusGoals: Dexie.Table<IDailyFocusGoal, number>;
+
+  constructor() {
+    super('MyDatabase');
+
+    this.version(1).stores({
+      preferences: '++id, pomodoroLength, shortBreakLength, longBreakLength, pomodoroCount',
+      focusStats: '++id, totalFocusTime, weeklyFocusTime, monthlyFocusTime',
+      dailyFocusGoals: '++id, date, focusGoal, focusStreak'
+    });
+
+    this.preferences = this.table('preferences');
+    this.focusStats = this.table('focusStats');
+    this.dailyFocusGoals = this.table('dailyFocusGoals');
+
+    // Populate the database with default preferences if none are found
+    this.preferences.count().then((count) => {
+      if (count === 0) {
+        this.preferences.add(DEFAULT_PREFERENCES);
+      }
+    });
+  }
 }
 
 const db = new MyDatabase();
@@ -37,20 +65,7 @@ const db = new MyDatabase();
 // Function to get preferences from the database, sets default preferences if none are found.
 export const getPreferences = async (): Promise<IPreference> => {
   const allPreferences = await db.preferences.toArray();
-
-  if (allPreferences.length === 0) {
-    console.log('No preferences found. Creating default preferences.');
-    const defaultPreferences: IPreference = {
-      pomodoroLength: 30,
-      shortBreakLength: 5,
-      longBreakLength: 15,
-      pomodoroCount: 4
-    };
-    await db.preferences.add(defaultPreferences);
-    return defaultPreferences;
-  } else {
-    return allPreferences[0]; // Assuming there's only one set of preferences.
-  }
+  return allPreferences[0]; // Assumes one set of preferences per user
 };
 
 // Function to get the latest focus time
@@ -60,8 +75,14 @@ export const getFocusTime = async (): Promise<number> => {
 };
 
 // Function to update the focus time
-export const updateFocusTime = async (newFocusTime: number): Promise<void> => {
-  await db.focusStats.add({ totalFocusTime: newFocusTime });
+export const updateFocusTime = async (
+  newFocusTime: number,
+  newDailyFocusGoal?: number
+): Promise<void> => {
+  await db.focusStats.update(1, { totalFocusTime: newFocusTime });
+  if (newDailyFocusGoal) {
+    await db.dailyFocusGoals.update(1, { focusGoal: newDailyFocusGoal });
+  }
 };
 
 // Use 'export type' for re-exporting types
