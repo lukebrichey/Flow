c375a4277d19e7ba4197deffabbd6d029f53d8e6
lukebrichey <lukebrichey@gmail.com>
2023-08-22 15:17:37 -0400

    feat: Added streak display to focus goal component

--- src/renderer/src/components/FocusGoal.tsx
+++ src/renderer/src/components/FocusGoal.tsx
@@ -1,4 +1,7 @@
 import ProgressBar from './ProgressBar';
+import { FireIcon } from '@heroicons/react/24/solid';
+import { useEffect, useState } from 'react';
+import { getFocusStreak } from '../lib/db';
 
 interface FocusGoalProps {
   focusTime: number;
@@ -6,11 +9,25 @@ interface FocusGoalProps {
 }
 
 export default function FocusGoal({ focusTime, focusGoal }: FocusGoalProps): JSX.Element {
+  const [streak, setStreak] = useState<number>(0);
+
+  // Fetch current streak from the database
+  useEffect(() => {
+    getFocusStreak().then((streak) => setStreak(streak));
+  }, []);
+
   return (
-    <div className="bg-white w-3/5 rounded-lg shadow-lg mx-auto">
-      <h3 className="text-center text-2xl font-medium pt-4">Daily Focus Goal: </h3>
+    <div className="bg-white w-3/5 rounded-lg shadow-lg mx-auto py-4">
+      <h3 className="text-center text-2xl font-medium">Daily Focus Goal: </h3>
       <ProgressBar value={focusTime} max={focusGoal} />
-      <p className="text-center text-lg pb-2">{`${focusTime}/${focusGoal}`} minutes</p>
+      <p className="text-center text-lg pb-2">
+        {`${((focusTime / focusGoal) * 100).toFixed(2)}%`} Complete
+      </p>
+      <h3 className="text-center text-2xl font-medium pt-4">Current Streak: </h3>
+      <div className="flex flex-row w-full justify-center items-center">
+        <p className="text-2xl">{streak}</p>
+        <FireIcon className="h-11 w-11 text-red-500" />
+      </div>
     </div>
   );
 }
